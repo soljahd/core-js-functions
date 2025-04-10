b7d699c965601a411ec0d9d5dfcb17822f872e96
soljah <moskuitoz@gmail.com>
2025-04-10 21:36:47 +0300

    feat: implemented retry, logger, partialUsingArguments, getIdGeneratorFunction functions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -142,8 +142,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function retryFn() {
+    let lastError;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        lastError = error;
+      }
+    }
+    throw lastError;
+  };
 }
 
 /**
@@ -169,8 +179,15 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function loggerFn(...args) {
+    const functionName = func.name;
+    const argsString = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${functionName}(${argsString}) starts`);
+    const result = func(...args);
+    logFunc(`${functionName}(${argsString}) ends`);
+    return result;
+  };
 }
 
 /**
@@ -186,8 +203,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function usingArguments(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -207,8 +226,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom - 1;
+  return function idGeneratorFunction() {
+    currentId += 1;
+    return currentId;
+  };
 }
 
 module.exports = {
